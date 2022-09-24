e34ebeb2f8699beffeee68ddd4acf406c1bbe7ed
Matthew Jakeman <mjakeman26@outlook.co.nz>
2022-09-25 12:35:54 +1300

    app: Populate list view

--- src/main/java/com/team01/scheduler/HelloApplication.java
+++ src/main/java/com/team01/scheduler/HelloApplication.java
@@ -17,58 +17,6 @@ import java.util.List;
 
 public class HelloApplication extends Application {
 
-    private TaskRunner taskRunner;
-
-    /**
-     * Creates a sample graph using the node and edge
-     * weights provided in the brief (refer to Figure 1).
-     *
-     * @return Graph instance
-     */
-    public Graph createSampleGraph() {
-        Node nodeA = new Node("a", 2);
-        Node nodeB = new Node("b", 3);
-        Node nodeC = new Node("c", 3);
-        Node nodeD = new Node("d", 2);
-
-        Edge edgeAB = new Edge(nodeA, nodeB, 1);
-        Edge edgeAC = new Edge(nodeA, nodeC, 2);
-        Edge edgeBD = new Edge(nodeB, nodeD, 2);
-        Edge edgeCD = new Edge(nodeC, nodeD, 1);
-
-        List<Node> nodes = Arrays.asList(nodeA, nodeB, nodeC, nodeD);
-        List<Edge> edges = Arrays.asList(edgeAB, edgeAC, edgeBD, edgeCD);
-
-        return new Graph(edges, nodes);
-    }
-
-    /**
-     * Creates a new task to print out all the edges in the graph.
-     *
-     * @return IRunnable instance
-     */
-    public IRunnable createPrintGraphTask() {
-        return new IRunnable() {
-            @Override
-            public String getTaskName() {
-                return "Print Graph";
-            }
-
-            @Override
-            public void run(Graph graph) {
-                for (EdgesLinkedList currentList: graph.getGraph().values()) {
-                    int listSize = currentList.size();
-
-                    for (int i = 0; i < listSize; i++) {
-                        Edge currentEdge = currentList.get(i);
-
-                        System.out.println(currentEdge.toString());
-                    }
-                }
-            }
-        };
-    }
-
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -76,14 +24,6 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-
-        Graph graphData = createSampleGraph();
-
-        // Do runner
-        taskRunner = new TaskRunner();
-
-        taskRunner.safeRun(createPrintGraphTask(), graphData);
-        taskRunner.safeRun(new DepthFirstSearch(), graphData);
     }
 
     public static void main(String[] args) {
--- src/main/java/com/team01/scheduler/HelloController.java
+++ src/main/java/com/team01/scheduler/HelloController.java
@@ -1,14 +1,66 @@
 package com.team01.scheduler;
 
+import com.team01.scheduler.algorithm.DepthFirstSearch;
+import com.team01.scheduler.algorithm.IRunnable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    private TaskRunner taskRunner;
+    private ObservableList<IRunnable> taskList;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ListView<IRunnable> listView;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    public void onRunTask(ActionEvent actionEvent) {
+        IRunnable runnable = listView.getSelectionModel().getSelectedItem();
+        taskRunner.safeRun(runnable, Utils.createSampleGraph());
+    }
+
+    public void initialize()
+    {
+        // Setup List View
+        listView.setItems(taskList);
+        listView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
+        // Cell Factory
+        // Refer to: https://stackoverflow.com/questions/36657299/how-can-i-populate-a-listview-in-javafx-using-custom-objects
+        listView.setCellFactory(task -> new ListCell<>() {
+            @Override
+            protected void updateItem(IRunnable item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty || item == null)
+                    setText(null);
+                else
+                    setText(item.getTaskName());
+            }
+        });
+    }
+
+    public HelloController() {
+
+        // Create runner
+        taskRunner = new TaskRunner();
+
+        // Add tasks
+        List<IRunnable> tasks = new ArrayList<>();
+
+        tasks.add(Utils.createPrintGraphTask());
+        tasks.add(new DepthFirstSearch());
+
+        taskList = FXCollections.observableList(tasks);
     }
 }
