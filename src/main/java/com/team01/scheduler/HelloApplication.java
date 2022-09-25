e8832f436934a1ecfcdd299808cff4abd2a1b4cc
JJTee <jiateec@gmail.com>
2022-09-25 15:36:53 +1300

    Export graph model to dot file

--- src/main/java/com/team01/scheduler/HelloApplication.java
+++ src/main/java/com/team01/scheduler/HelloApplication.java
@@ -4,6 +4,7 @@ import com.digraph.weighted.models.Edge;
 import com.digraph.weighted.models.EdgesLinkedList;
 import com.digraph.weighted.models.Graph;
 import com.digraph.weighted.models.Node;
+import com.digraph.weighted.util.ExportToDotFile;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -23,38 +24,38 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) throws IOException {
-//        List<Node> nodes = new ArrayList<>();
-//        Node a = new Node("a", 2);
-//        Node b = new Node("b", 3);
-//        Node c = new Node("c", 1);
-//        Node d = new Node("d", 2);
-//        nodes.add(a);
-//        nodes.add(b);
-//        nodes.add(c);
-//        nodes.add(d);
-//
-//        List<Edge> edges = new ArrayList<>();
-//        edges.add(new Edge(a, b, 1));
-//        edges.add(new Edge(a, c, 2));
-//        edges.add(new Edge(b, d, 2));
-//        edges.add(new Edge(c, d,1));
-//
-//        Graph graph = new Graph(edges, nodes);
-//
-//        Map<Node, EdgesLinkedList> graphStructure = new HashMap<>();
-//        graphStructure = graph.getGraph();
-//
-//        for (EdgesLinkedList currentList: graphStructure.values()) {
-//            int listSize = currentList.size();
-//            Edge currentEdge = currentList.get(0);
-//
-//            for (int i = 0; i < listSize; i++) {
-//                currentEdge = currentList.get(i);
-//
-//                System.out.println(currentEdge.toString());
-//            }
-//        }
+        List<Node> nodes = new ArrayList<>();
+        Node a = new Node("a", 2);
+        Node b = new Node("b", 3);
+        Node c = new Node("c", 1);
+        Node d = new Node("d", 2);
+        nodes.add(a);
+        nodes.add(b);
+        nodes.add(c);
+        nodes.add(d);
 
+        List<Edge> edges = new ArrayList<>();
+        edges.add(new Edge(a, b, 1));
+        edges.add(new Edge(a, c, 2));
+        edges.add(new Edge(b, d, 2));
+        edges.add(new Edge(c, d,1));
+
+        Graph graph = new Graph(edges, nodes);
+
+        Map<Node, EdgesLinkedList> graphStructure = new HashMap<>();
+        graphStructure = graph.getGraph();
+
+        for (EdgesLinkedList currentList: graphStructure.values()) {
+            int listSize = currentList.size();
+            Edge currentEdge = currentList.get(0);
+
+            for (int i = 0; i < listSize; i++) {
+                currentEdge = currentList.get(i);
+
+                System.out.println(currentEdge.toString());
+            }
+        }
+/*
         List<Node> nodes = new ArrayList<>();
         List<Edge> edges = new ArrayList<>();
 
@@ -111,8 +112,9 @@ public class HelloApplication extends Application {
 
                 System.out.println(currentEdge.toString());
             }
-        }
-
+        }*/
+        ExportToDotFile ex = new ExportToDotFile(graph);
+        ex.writeDot();
         launch();
     }
 }
