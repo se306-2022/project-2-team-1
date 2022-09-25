dd0fa843dc4878bb65abb4c55f76323c0e0e0c4c
Matthew Jakeman <mjakeman26@outlook.co.nz>
2022-09-25 13:13:04 +1300

    console: Add internal console to test app

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.team01.scheduler {
     requires google.options;
     requires org.slf4j;
 
-
     opens com.team01.scheduler to javafx.fxml;
+    opens com.team01.scheduler.gui to javafx.fxml;
     exports com.team01.scheduler;
 }
