f34d064898b5c3b2ff4ae795d8caabb7429a13ec
Bryan LIu <bliu631@aucklanduni.ac.nz>
2022-10-02 22:13:17 +1300

    Created tests for graph parser
    Tests for cyclic exception, edge weight exception, and invalid name exception

--- scheduler-core/src/test/java/com/team01/graphparser/GraphParserTest.java
+++ scheduler-core/src/test/java/com/team01/graphparser/GraphParserTest.java
@@ -1,10 +1,15 @@
 package com.team01.graphparser;
 
 import com.team01.scheduler.Utils;
+import com.team01.scheduler.graph.exceptions.CycleException;
+import com.team01.scheduler.graph.exceptions.InvalidEdgeWeightException;
+import com.team01.scheduler.graph.exceptions.InvalidInputException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class GraphModelTest {
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class GraphParserTest {
 
 
     @Test
@@ -15,6 +20,30 @@ class GraphModelTest {
 
         Assertions.assertNull(graph);
     }
+
+    @Test
+    void testCyclicGraphException() {
+
+        CycleException exception = assertThrows(CycleException.class, () -> {
+            String name = "cyclic_graph.dot";
+            String graph = Utils.loadResource(Utils.class, name);
+        });
+
+        Assertions.assertEquals(exception, new CycleException("Graph is cyclic."));
+    }
+
+    @Test
+    void testEdgeWeightException() {
+
+        InvalidEdgeWeightException exception = assertThrows(InvalidEdgeWeightException.class, () -> {
+            String name = "invalid_edge_weight_exception.dot";
+            String graph = Utils.loadResource(Utils.class, name);
+        });
+
+        Assertions.assertEquals(exception, new InvalidEdgeWeightException("Graph has invalid edge weight."));
+    }
+
+
 
 
 }
