7e4882bb414fd3ecf62aae9e3d5ff67486d15ba3
Bryan LIu <bliu631@aucklanduni.ac.nz>
2022-10-02 20:28:27 +1300

    Update loadResource method to take name from parameter rather than hardcode

--- scheduler-core/src/main/java/com/team01/scheduler/Utils.java
+++ scheduler-core/src/main/java/com/team01/scheduler/Utils.java
@@ -77,7 +77,7 @@ public class Utils {
      */
     public static <T> String loadResource(Class<T> klass, String name) {
         // Get input stream for resource
-        InputStream inputStream = klass.getClassLoader().getResourceAsStream("Nodes_7_OutTree.dot");
+        InputStream inputStream = klass.getClassLoader().getResourceAsStream(name);
 
         if (inputStream != null) {
             // Try read the resource input stream
@@ -89,6 +89,7 @@ public class Utils {
                 while ((append = reader.readLine()) != null)
                     contents.append(append).append("\n");
 
+                System.out.println(contents);
                 return contents.toString();
             } catch (Exception e) {
                 e.printStackTrace();
