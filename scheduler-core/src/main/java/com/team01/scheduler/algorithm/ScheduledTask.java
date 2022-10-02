a40dc9286df8f3fb69799669dd11d104139ce20d
Bryan LIu <bliu631@aucklanduni.ac.nz>
2022-10-03 00:20:49 +1300

    Added comments

--- scheduler-core/src/main/java/com/team01/scheduler/algorithm/ScheduledTask.java
+++ scheduler-core/src/main/java/com/team01/scheduler/algorithm/ScheduledTask.java
@@ -2,12 +2,25 @@ package com.team01.scheduler.algorithm;
 
 import com.team01.scheduler.graph.models.Node;
 
+/**
+ * Each task in this class has a processor ID associated with it as well as the node in question. Each
+ * task also has the earliest start time.
+ */
 public class ScheduledTask {
     int startTime;
     int processorId;
     Node node;
     ScheduledTask parent;
 
+    /**
+     * The constructor to create ScheduledTask. Multiple instances are created
+     * when looking at all child nodes in the queue.
+     *
+     * @param parent        The parent scheduled task preceding the current node
+     * @param startTime     The available starting time of the current node
+     * @param processorId   The processor associated with the current node
+     * @param node          The current node
+     */
     public ScheduledTask(ScheduledTask parent, int startTime, int processorId, Node node) {
         this.startTime = startTime;
         this.processorId = processorId;
@@ -15,6 +28,11 @@ public class ScheduledTask {
         this.parent = parent;
     }
 
+    /**
+     * Formatter function to display progress of algorithm
+     *
+     * @return  The formatted output
+     */
     @Override
     public String toString() {
         return "processorId: " + getProcessorId() + " | startTime: " + getStartTime() + " | node: " + getNode().getName();
